0647fa6d30ccbee69c73c4d144e5c431ce81ee5e
rodexiy <rodexcontato@gmail.com>
2023-04-03 16:51:33 -0300

    aa
    aa

--- src/ServerScriptService/services/DataManager.lua
+++ src/ServerScriptService/services/DataManager.lua
@@ -15,5 +15,20 @@ function DataService:Get(player: Player)
 	return data
 end
 
+function DataService:GetTemporaryData(player: Player)
+	
+end
+
+function DataService:GetDataFromCharacter(character: Model)
+	if not game:GetService("Players"):GetPlayerFromCharacter(character) then
+		return require(character.Data)
+	else
+		return DataService:Get(game:GetService("Players"):GetPlayerFromCharacter(character))
+	end
+end
+
+function DataService:GetTemporaryDataFromCharacter(character: Model)
+	
+end
 
 return DataService
